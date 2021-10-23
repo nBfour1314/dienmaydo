fdf374ae0bc19ce23946a9a256d75a7a7b8359a2
nBfour1314 <nguyennhathao11c2@gmail.com>
2021-10-23 07:46:23 +0700

    code dien may do 1

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 	
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-	$Connect = pg_connect("postgres://jhzewzqcikbaox:404963d9604e1177900087ba6ac1166d9c7df951af9f7af099ddf430d8e621b5@ec2-52-204-14-80.compute-1.amazonaws.com:5432/d6h40h1o0qarbi");
+	$Connect = pg_connect("postgres://rrlchliooolmkv:06d182efd93d710eaadefb73e7c17fc16a88b181a449694c6cdbbd63faac998b@ec2-18-234-15-247.compute-1.amazonaws.com:5432/dcclnqfgv58qht");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	
